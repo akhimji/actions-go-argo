77e1b475d8ce59e4a7537ce2f3ffc06ff1cd7054
alyarctiq <aly.khimji@artiq.ca>
2019-12-07 16:27:28 -0500

    builder

--- builder/deploy-update.sh
+++ builder/deploy-update.sh
@@ -1,4 +1,5 @@
 TAG=$(echo $GITHUB_SHA | head -c7) 
-sed -i 's|<IMAGE-DJ>|r00tsh3ll/actionsgo-dadjoke:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yml 
-sed -i 's|<IMAGE-BEER>|r00tsh3ll/actionsgo-randombeer:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yml 
-sed -i 's|<IMAGE-UI>|r00tsh3ll/actionsgo-dadjoke:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yml 
+sed -i 's|<IMAGE-DJ>|r00tsh3ll/actionsgo-dadjoke:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yaml 
+sed -i 's|<IMAGE-BEER>|r00tsh3ll/actionsgo-randombeer:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yaml 
+sed -i 's|<IMAGE-UI>|r00tsh3ll/actionsgo-dadjoke:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yaml 
+sed -i 's|<IMAGE-SIMPSONS>|r00tsh3ll/actionsgo-simpons:'${TAG}'|' $GITHUB_WORKSPACE/deployment/deployment.yaml
